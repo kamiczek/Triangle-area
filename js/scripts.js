4d3d823b66f56bdac78bfa701f289a6df8056c35
Remigiusz Rajca <remigiusz.rajca@gmail.com>
2017-07-05 20:47:11 +0200

    changed variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var a = prompt('Triangle width: ');
-    h = prompt('Triangle height: ');
-    triangleArea = a*h/2;
+var h = prompt('Triangle height: ');
+var triangleArea = a*h/2;
+
 alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
